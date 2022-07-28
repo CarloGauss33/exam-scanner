e44535a34d5ee7539a93930e26cf746fad99461e
Carlos Paredes <cparedesr@uc.cl>
2022-07-28 11:28:57 -0400

    feat(): mobile fix

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <script setup lang="ts">
   import { jsPDF } from "jspdf";
   import { ref } from "vue";
-  import exif from 'exif-js';
 
   const images = ref<string[]>([]);
   const numberOfAttachments = ref(1);
+  const isGenerating = ref(false);
+
+  function submit() {
+    isGenerating.value = true;
+    generatePDF();
+  }
 
   function generatePDF() {
+    isGenerating.value = true;
     const doc = new jsPDF('p', 'mm','a4', true);
+    var canvas = document.createElement("canvas") as HTMLCanvasElement;
+    const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+    var img = new Image();
+
     images.value.forEach((image, index) => {
       const alias = `image${index}`;
-      const imageData = new Image();
-      imageData.src = image;
-      const isPortrait = imageData.height > imageData.width;
-      if (isPortrait) {
-        doc.addImage(imageData, 'JPEG', 0, 0, 210, 297);
-      } else {
-        doc.addImage(image, "JPEG", 0, -210, 297, 210, alias, 'NONE', 270);
-      }
-      if (index < numberOfAttachments.value - 1) {
-        doc.addPage();
-      }
+      img.src = image;
+      img.onload = () => {
+        doc.setFontSize(7);
+        doc.text("Page " + (index + 1), 200, 2);
+        canvas.width = img.width;
+        canvas.height = img.height;
+        context?.drawImage(img, 0, 0);
+        doc.addImage(canvas.toDataURL('image/png'), 'PNG', 3.5, 3.5, 203, 290, alias, "SLOW");
+        if (index < images.value.length - 1) {
+          doc.addPage();
+        }
+      };
     });
-    doc.save("test.pdf");
+    setTimeout(() => {
+      doc.save('test.pdf');
+    }, 2000);
+    isGenerating.value = false;
   }
 
   function onFileChange(event: Event, index: number) {
@@ -39,16 +53,20 @@
 </script>
 
 <template>
-  <div class="flex flex-col">
-    <form @submit.prevent="generatePDF">
-      <div v-for="i in numberOfAttachments" :key="i">
-        <input type="file" :id="`file-field-${i}`" @change="onFileChange($event, i-1)" accept="image/*" capture="environment">
+  <div class="flex flex-col space-y-4 md:p-24 p-4 bg-white min-h-screen w-screen">
+    <form @submit.prevent="submit" class="bg-slate-300 md:p-8 p-3 rounded-md w-full">
+      <div v-for="i in numberOfAttachments" :key="i" class="mb-6">
+        <input type="file" :id="`file-field-${i}`" @change="onFileChange($event, i-1)" accept="image/jpg" capture="environment" class="mb-0.5">
       </div>
-      <button type="button" @click="numberOfAttachments = numberOfAttachments + 1" class="btn btn-primary">
-        Add file
-      </button>
-      <br/>
-      <button type="submit" class="btn btn-primary">Generate PDF</button>
+      <div class="flex flex-row items-center space-x-5">
+        <button type="button" @click="numberOfAttachments = numberOfAttachments + 1" class="p-1 bg-gray-400 hover:bg-gray-500 rounded-md">
+          Add file
+        </button>
+        <button type="submit" class="p-1 bg-gray-400 hover:bg-gray-500 rounded-lg">Generate PDF</button>
+      </div>
+      <div v-if="isGenerating">
+        <p class="text-center">Generating PDF...</p>
+      </div>
     </form>
   </div>
 </template>
