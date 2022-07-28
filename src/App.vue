c6ad011326b3bf8e81decaf00346e24178cdd575
Carlos Paredes <cparedesr@uc.cl>
2022-07-28 02:56:20 -0400

    feat(): fix rotation in android and ios

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
   import { jsPDF } from "jspdf";
   import { ref } from "vue";
+  import exif from 'exif-js';
 
   const images = ref<string[]>([]);
   const numberOfAttachments = ref(1);
 
   function generatePDF() {
-    const doc = new jsPDF('p', 'mm','a4',true);
+    const doc = new jsPDF('p', 'mm','a4', true);
     images.value.forEach((image, index) => {
-      doc.addImage(image, "JPEG", 0, 0, 210, 297);
+      const alias = `image${index}`;
+      const imageData = new Image();
+      imageData.src = image;
+      const isPortrait = imageData.height > imageData.width;
+      if (isPortrait) {
+        doc.addImage(imageData, 'JPEG', 0, 0, 210, 297);
+      } else {
+        doc.addImage(image, "JPEG", 0, -210, 297, 210, alias, 'NONE', 270);
+      }
       if (index < numberOfAttachments.value - 1) {
         doc.addPage();
       }
@@ -16,19 +25,15 @@
     doc.save("test.pdf");
   }
 
-  function onFileChange(event: Event) {
-    // add file to images array
+  function onFileChange(event: Event, index: number) {
     event.preventDefault();
-    const files = (event.target as HTMLInputElement).files;
-    if (files) {
-      console.log(files);
-      for (let i = 0; i < files.length; i++) {
-        const reader = new FileReader();
-        reader.onload = (e: any) => {
-          images.value.push(e.target.result);
-        };
-        reader.readAsDataURL(files[i]);
-      }
+    const file = (event.target as HTMLInputElement).files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        images.value[index] = e.target.result;
+      };
+      reader.readAsDataURL(file);
     }
   }
 </script>
@@ -37,7 +42,7 @@
   <div class="flex flex-col">
     <form @submit.prevent="generatePDF">
       <div v-for="i in numberOfAttachments" :key="i">
-        <input type="file" :id="`file-field-${i}`" @change="onFileChange" accept="image/*" />
+        <input type="file" :id="`file-field-${i}`" @change="onFileChange($event, i-1)" accept="image/*" capture="environment">
       </div>
       <button type="button" @click="numberOfAttachments = numberOfAttachments + 1" class="btn btn-primary">
         Add file
@@ -45,6 +50,5 @@
       <br/>
       <button type="submit" class="btn btn-primary">Generate PDF</button>
     </form>
-
   </div>
 </template>
