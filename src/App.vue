a7381333252c7c433e5b3d0467a1e0fdddf042d3
Carlos Paredes <cparedesr@uc.cl>
2022-08-09 01:42:12 -0400

    feat(): is working

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <script setup lang="ts">
   import { jsPDF } from "jspdf";
-  import { ref } from "vue";
+  import { ref, computed } from "vue";
+  import * as imageConversion from 'image-conversion';
 
   const images = ref<string[]>([]);
+  const inputRendering = ref<boolean[]>([false]);
   const numberOfAttachments = ref(1);
   const isGenerating = ref(false);
+
+  function showRandomMessage() {
+    const options = ['Procesando la imagen..',
+                     'Comprimiendo...',
+                     'Haciendo cosas...',
+                     'Aplicando filtros...',
+                     'Llamando a Dios...'];
+    return options[Math.floor(Math.random() * options.length)];
+  }
+
+  const isAnyInputRendering = computed(() => {
+    return inputRendering.value.some(x => x);
+  });
+
+
   function submit() {
     isGenerating.value = true;
 
@@ -16,48 +33,45 @@
 
   async function generatePDF() {
     const doc = new jsPDF('p', 'mm','a4', true);
-    var canvas = document.createElement("canvas") as HTMLCanvasElement;
-    const context = canvas.getContext('2d') as CanvasRenderingContext2D;
 
     images.value.forEach((image, index) => {
       const alias = `image${index}`;
-      new Promise( resolve => {
-        var img = new Image();
-        img.src = image;
-        resolve(img as HTMLImageElement);
-      }).then( img => {
-        doc.setFontSize(7);
-        doc.text("Page " + (index + 1), 200, 2);
-        canvas.width = img.width;
-        canvas.height = img.height;
-        context?.drawImage(img, 0, 0);
-        doc.addImage(canvas.toDataURL('image/png'), 'PNG', 3.5, 3.5, 203, 290, alias, "SLOW");
+      doc.setFontSize(7);
+      doc.text("Page " + (index + 1), 200, 2);
+      doc.addImage(image, 'JPEG', 3.5, 3.5, 203, 290, alias, "SLOW");
 
-        if (index === images.value.length - 1) {
-          doc.save('test.pdf');
-        } else {
-          doc.addPage();
-        }
-      });
+      if (index === images.value.length - 1) {
+        doc.save('test.pdf');
+      } else {
+        doc.addPage();
+      }
     });
   }
 
-  function onFileChange(event: Event, index: number) {
-    event.preventDefault();
-    const file = (event.target as HTMLInputElement).files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (e: any) => {
-        images.value[index] = e.target.result;
-      };
-      reader.readAsDataURL(file);
-    }
+  async function onFileChange(event: Event, index: number) {
+    inputRendering.value[index] = true;
+
+    const file =  (event.target as HTMLInputElement).files[0];
+
+    imageConversion.compress(
+      file,
+      {
+        quality: 0.3,
+        type: "image/jpeg"
+      }).then(res=>{
+      var reader = new FileReader();
+      reader.readAsDataURL(res);
+      reader.onload = function () {
+        images.value[index] = reader.result as string;
+        inputRendering.value[index] = false;
+      }
+    })
   }
 </script>
 
 <template>
   <div class="flex flex-col space-y-4 md:p-24 p-4 bg-white min-h-screen w-screen">
-    <form class="bg-slate-300 md:p-8 p-3 rounded-md w-full">
+    <div class="bg-slate-300 md:p-8 p-3 rounded-md w-full">
       <div v-for="i in numberOfAttachments" :key="i" class="mb-6">
         <input type="file" :id="`file-field-${i}`" @change="onFileChange($event, i-1)" accept="image/jpg" capture="environment" class="mb-0.5">
       </div>
@@ -65,11 +79,16 @@
         <button type="button" @click="numberOfAttachments = numberOfAttachments + 1" class="p-1 bg-gray-400 hover:bg-gray-500 rounded-md">
           Add file
         </button>
-        <button type="button" @click="submit" class="p-1 bg-gray-400 hover:bg-gray-500 rounded-lg">Generate PDF</button>
+        <button type="button" @click="submit" class="p-1 bg-gray-400 hover:bg-gray-500 rounded-lg" :class="{'opacity-50': isAnyInputRendering}" :disabled="isAnyInputRendering">
+          Generate
+        </button>
       </div>
-      <div v-if="isGenerating">
-        <p>Generating PDF...</p>
+      <div v-if="isAnyInputRendering" class="mt-5 text-center text-black font-bold">
+        <p>{{ showRandomMessage() }}</p>
       </div>
-    </form>
+      <div v-if="isGenerating" class="mt-5 text-center text-black font-bold">
+        <p>{{ showRandomMessage() }}</p>
+      </div>
+    </div>
   </div>
 </template>
---
+++ src/components/Scanner.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+  import { jsPDF } from "jspdf";
+  import { ref } from "vue";
+
+  const images = ref<string[]>([]);
+  const canvas = ref<HTMLCanvasElement[]>([]);
+  const numberOfAttachments = ref(1);
+  const isGenerating = ref(false);
+  const isLoadingImages = ref(false);
+  function submit() {
+    isGenerating.value = true;
+
+    generatePDF();
+
+  }
+
+  function generatePDF() {
+    const doc = new jsPDF('p', 'mm','a4', true);
+
+    images.value.forEach((image, index) => {
+      const alias = `image${index}`;
+      doc.setFontSize(7);
+      doc.text("Page " + (index + 1), 200, 2);
+      doc.addImage(image, 'PNG', 3.5, 3.5, 203, 290, alias, "SLOW");
+
+      if (index === images.value.length - 1) {
+        doc.save('test.pdf');
+      } else {
+        doc.addPage();
+      }
+    });
+  }
+
+  function onFileChange(event: Event, index: number) {
+    isLoadingImages.value = true;
+    event.preventDefault();
+    const file = (event.target as HTMLInputElement).files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        var canvas = document.createElement("canvas") as HTMLCanvasElement;
+        const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+        const img = new Image();
+        img.src = e.target.result;
+        img.onload = () => {
+          canvas.width = img.width;
+          canvas.height = img.height;
+          context?.drawImage(img, 0, 0);
+          images.value[index] = canvas.toDataURL('image/png');
+          isLoadingImages.value = false;
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+</script>
+
+<template>
+  <div class="flex flex-col space-y-4 md:p-24 p-4 bg-white min-h-screen w-screen">
+    <form class="bg-slate-300 md:p-8 p-3 rounded-md w-full">
+      <div v-for="i in numberOfAttachments" :key="i" class="mb-6">
+        <input type="file" :id="`file-field-${i}`" @change="onFileChange($event, i-1)" accept="image/jpg" capture="environment" class="mb-0.5">
+      </div>
+      <div class="flex flex-row items-center space-x-5">
+        <button type="button" @click="numberOfAttachments = numberOfAttachments + 1" class="p-1 bg-gray-400 hover:bg-gray-500 rounded-md">
+          Add file
+        </button>
+        <button type="button" @click="submit" class="p-1 bg-gray-400 hover:bg-gray-500 rounded-lg">Generate PDF</button>
+      </div>
+      <div v-if="isGenerating">
+        <p>Generating PDF...</p>
+      </div>
+    </form>
+  </div>
+</template>
